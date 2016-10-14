6b79712a778af016c11033ec608a8144f7ad9017
zell92 <n.sardella@hotmail.it>
2016-10-14 17:17:32 +0200

    completato

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -1,16 +1,33 @@
 from pyplasm import *
-""" prova"""
+"""this function returns some HTC values, representative of a space frame. 
+Input: 
+bx,bz (given dimensions of beam section), 
+px,py (given dimensions of pillar section), 
+[dy1,dy2,...] (distances between axes of the pillars), 
+[hz1,hz2,...] (interstory heights)"""
 def spaceFrame(bx,bz,px,py,distances,heights):
 
   d=distances[0]*-1;
   h=heights[0];
 
-  pilX=[px]
+  if bx>px:
+    dist=(bx-px)/2.
+    pilX=[0,-dist,px]
+    beaX=[bx]
+  elif bx==px:
+    beaX=[bx]
+    pilX=[px]
+  else:
+    dist=(px-bx)/2.
+    beaX=[0,-dist,bx]
+    pilX=[px]
+
+  
   pilY=[py]
   pilZ=[h]
 
-  beaX=[bx]
-  beaY=[0,-py/2]
+  
+  beaY=[py/2,0]
   beaZ=[0,h*-1,bz]
 
   
@@ -29,25 +46,22 @@ def spaceFrame(bx,bz,px,py,distances,heights):
     pilZ.append(h)
     beaZ.append(h*-1)
     beaZ.append(bz)
+  beaY.append(py/2)
 
-  a=QUOTE(pilX)
-  b=QUOTE(pilY)
-  c=QUOTE(pilZ)
-  q=QUOTE(beaX)
-  w=QUOTE(beaY)
-  e=QUOTE (beaZ)
-  bb=PROD([q,w])
-  bbb=PROD([bb,e])
-  aa=PROD([a,b])
-  aaa=PROD([aa,c])
-  ccc=STRUCT([aaa,bbb])
-  return ccc
+  pillarX=QUOTE(pilX)
+  pillarY=QUOTE(pilY)
+  pillarZ=QUOTE(pilZ)
+  beamsX=QUOTE(beaX)
+  beamsY=QUOTE(beaY)
+  beamsZ=QUOTE (beaZ)
+  beams2D=PROD([beamsX,beamsY])
+  beams3D=PROD([beams2D,beamsZ])
+  pillars2D=PROD([pillarX,pillarY])
+  pillars3D=PROD([pillars2D,pillarZ])
+  combine=STRUCT([beams3D,pillars3D])
+  return combine
   
 
     
-
-
-d = [4,3]
-h=[4,3]
-VIEW(spaceFrame(1.,1.,1.,1.,[4,1],[4,3]))
+VIEW(spaceFrame(1.,1.,1.,1.,[4,1,5,2],[4,2,5]))
 
